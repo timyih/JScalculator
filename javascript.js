e831ca1dfa5bbb459d76e79e67266df421cdaaaf
Tim Yih <timmyyih@gmail.com>
2022-10-14 16:47:48 -0400

    Create basic operations and operate functions

---
+++ javascript.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2; 
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    if (operator == '+') {
+        return add(num1, num2);
+    }
+    if (operator == '-') {
+        return subtract(num1, num2);
+    }
+    if (operator == '*') {
+        return multiply(num1, num2);
+    }
+    if (operator == '/') {
+        return divide(num1, num2);
+    }
+}
+
