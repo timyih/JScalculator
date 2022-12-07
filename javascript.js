ebbf7b53cf8230cd629b27bf074b877b8900221f
Tim Yih <timmyyih@gmail.com>
2022-12-07 03:14:57 -0500

    Fix running algorithm and calculation functionality

--- javascript.js
+++ javascript.js
@@ -1,17 +1,22 @@
+let displayvalue = "";
+let currNum = "";
+let nextNum = "";
+let op = "";
+
 function add(num1, num2) {
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return parseInt(num1) - parseInt(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2; 
+    return parseInt(num1) * parseInt(num2); 
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return parseInt(num1) / parseInt(num2);
 }
 
 function operate(operator, num1, num2) {
@@ -29,3 +34,50 @@ function operate(operator, num1, num2) {
     }
 }
 
+function run() {
+    const buttons = document.querySelectorAll('button.num');
+    const inputField = document.getElementById('calcInput');
+    buttons.forEach(button => button.addEventListener('click', () => {
+        if (op == "") {
+            currNum = currNum + button.textContent;
+            displayvalue = currNum;
+        } else {
+            display = nextNum;
+            nextNum = nextNum + button.textContent;
+            displayvalue = nextNum;
+        };
+        inputField.innerHTML = displayvalue;
+    }));
+
+    const operations = document.querySelectorAll('button.op');
+    operations.forEach(button => button.addEventListener('click', () => {
+        op = button.textContent;
+        if(nextNum != ""){
+            result = operate(op, currNum, nextNum);
+            displayvalue = result;
+            currNum = result;
+            nextNum = "";
+        }
+        inputField.innerHTML = displayvalue;
+    }));
+
+    const clearbtn = document.getElementById('clrbtn');
+    clearbtn.addEventListener('click', () => {
+        displayvalue = "";
+        currNum = "";
+        nextNum = "";
+        inputField.innerHTML = displayvalue;
+    });
+
+    const eval = document.getElementById('eval');
+    eval.addEventListener('click', () => {
+        result = operate(op, currNum, nextNum);
+        displayvalue = result;
+        currNum = result;
+        nextNum = "";
+        op = "";
+        inputField.innerHTML = displayvalue;
+    });
+}
+
+run();
